6573a67dd760bd6512322283134475dc1946dace
arun <arun.hbni@gmail.com>
2019-12-07 07:27:07 +0530

    x

---
+++ check.sh
@@ -0,0 +1,5 @@
+./node_modules/jshint/bin/jshint --show-non-errors https.js
+./node_modules/.bin/eslint https.js
+
+./node_modules/jshint/bin/jshint --show-non-errors crl.js
+./node_modules/.bin/eslint crl.js
