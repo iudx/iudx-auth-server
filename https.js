92623196f9d9bc8bd145d4b45fbe588bcbe4b9d6
arun <arun.hbni At  gmail.com>
2019-12-11 10:59:16 +0530

    x

--- https.js
+++ https.js
@@ -1054,7 +1054,7 @@ app.all('/auth/v1/token', function (req, res) {
 			"token"		: SERVER_NAME + "/" + consumer_id + "/" + token,
 			"token-type"	: "IUDX",
 			"expires-in"	: token_time,
-			"server-token"	: "{}",
+			"server-token"	: {},
 		};
 
 		const num_resource_servers = Object.keys(resource_server_token).length;
