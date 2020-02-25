994ac27a8c7e8e2d7e54e2cfd73df44a1478300e
ThorodanBrom <bpr_95@hotmail.com>
2020-02-25 16:57:58 +0530

    Fix

--- test/init.py
+++ test/init.py
@@ -12,5 +12,4 @@ else:
 
 consumer		= Auth("consumer.pem", private_key, auth_server)
 provider		= Auth("provider.pem", private_key, auth_server)
-resource_server		= Auth("r-server.pem", private_key, auth_server)
 fake_resource_server	= Auth("f-server.pem", private_key, auth_server)
