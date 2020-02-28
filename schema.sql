f769e17de5e91e26c9b85fcc2bb0a1cec30ce6e1
arun <arun.hbni@gmail.com>
2020-02-28 14:06:45 +0530

    add timezone to psql

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 11.2
 -- Dumped by pg_dump version 11.2
 
+SET TIMEZONE='Asia/Kolkata';
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
