9a18bcd647a1fd3435e0319ae61dadd8908efb4d
arun-babu <arun.hbni@gmail.com>
2020-03-17 21:35:32 +0530

    x

--- test/test-certificates.py
+++ test/test-certificates.py
@@ -6,11 +6,14 @@ from init import *
 r = resource_server.certificate_info()
 assert r["success"] == True
 assert r["response"]["certificate-class"] == 1
+assert r["response"]["id"] == "arun.babu@rbccps.org" 
 
 r = consumer.certificate_info()
 assert r["success"] == True
 assert r["response"]["certificate-class"] == 2
+assert r["response"]["id"] == "barun@iisc.ac.in" 
 
 r = provider.certificate_info()
 assert r["success"] == True
 assert r["response"]["certificate-class"] == 3
+assert r["response"]["id"] == "example@example.com" 
