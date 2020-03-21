1fc7889372671b46582c2e5e579303d494a41bff
arun-babu <arun.hbni@gmail.com>
2020-03-21 19:38:38 +0530

    fix a bug

--- test/init.py
+++ test/init.py
@@ -11,7 +11,7 @@ if "AUTH_SERVER" in os.environ and os.environ["AUTH_SERVER"] == "localhost":
     import urllib3
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     auth_server = "localhost"
-    resource_server = auth.Auth(home + "l-server.pem", home + "l-server.key.pem", auth_server)
+    resource_server = Auth(home + "l-server.pem", home + "l-server.key.pem", auth_server)
 #
 else:
 #
