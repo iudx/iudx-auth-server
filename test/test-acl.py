cef801e2872c8c74ff226b18aaa75d78e5b6ed28
arun-babu <arun.hbni@gmail.com>
2020-03-24 13:35:55 +0530

    update tests

--- test/test-acl.py
+++ test/test-acl.py
@@ -6,8 +6,8 @@ rules = [
 	'x@x.com can access rs1.com/x/y/z/t/a/b/c for 2 days',
 	'x@x.com can access rs1.com/_x/y/z/t/a/b/c for 2 days if country = "IN" AND api = "/latest"',
 	'x@x.com can access rs1.com/x-t/y/z/t/a/b/c for 2 days if country = "IN" OR  api = "/latest"',
-	'consumer1@domain.com and consumer2@domain.com can access rs1.com/x @ 10 INR for 5 hours',
-	'a,b@b.com, and c can access x/y/z.a.b.c/t @ 10 INR for 2 seconds; all can access anything; x can access y',
+	'consumer1@domain.com and consumer2@domain.com can access rs1.com/x for 5 hours @ 5 INR',
+	'a,b@b.com, and c can access x/y/z.a.b.c/t for 2 seconds @ 10.5 INR; all can access anything; x can access y',
 ]
 
 for rule in rules:
