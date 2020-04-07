d31a5f328271640b74147f6a640ad6b61525c8cb
arun-babu <arun.hbni@gmail.com>
2020-04-07 13:59:12 +0530

    more test cases

--- test/test-auth.py
+++ test/test-auth.py
@@ -10,18 +10,88 @@ RS = "iisc.iudx.org.in"
 if "AUTH_SERVER" in os.environ and os.environ["AUTH_SERVER"] == "localhost":
     RS = "localhost"
 
+r = consumer.get_policy()
+assert r['success']	is False
+assert r['status_code'] == 403 
+
 policy = "x can access *" # dummy policy
 r = consumer.set_policy(policy)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = consumer.append_policy(policy)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.get_policy()
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.set_policy(policy)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.append_policy(policy)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+tokens		= ["dummy"]
+token_hashes	= ["dummy"]
+
+r = resource_server.revoke_tokens(tokens)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.revoke_token_hashes(tokens)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.revoke_all("invalid","invalid")
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.audit_tokens(10)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.add_consumer_to_group("arun","confidential",20)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.delete_consumer_from_group("arun","confidential")
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = resource_server.list_group("confidential")
 assert r['success']	is False
 assert r['status_code'] == 403 
 
 token 		= {}
 server_token	= {}
 
+r = provider.introspect_token (token,server_token)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+# consumers
+
 r = consumer.introspect_token (token,server_token)
 assert r['success']	is False
 assert r['status_code'] == 403 
 
-r = provider.introspect_token (token,server_token)
+r = consumer.introspect_token (token,server_token)
 assert r['success']	is False
 assert r['status_code'] == 403 
+
+r = consumer.add_consumer_to_group("arun","confidential",20)
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = consumer.delete_consumer_from_group("arun","confidential")
+assert r['success']	is False
+assert r['status_code'] == 403 
+
+r = consumer.list_group("confidential")
+assert r['success']	is False
+assert r['status_code'] == 403 
+
