c3c16dfe192d5d725b2a77db6f90b1e696b7ba82
arun-babu <arun.hbni@gmail.com>
2020-04-12 17:34:49 +0530

    set localhost

--- test/set-local.sh
+++ test/set-local.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "lookup file bind"    > /etc/resolv.conf
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+echo "lookup file bind"    		>  /etc/resolv.conf
+echo "nameserver 1.1.1.1"		>> /etc/resolv.conf
 
-echo "127.0.0.1	auth.iudx.org.in" >> /etc/hosts
-echo "127.0.0.1	iisc.iudx.org.in" >> /etc/hosts
+echo "127.0.0.1	auth.iudx.org.in"	>> /etc/hosts
+echo "127.0.0.1	iisc.iudx.org.in"	>> /etc/hosts
+echo "127.0.0.1	localhost"		>> /etc/hosts
